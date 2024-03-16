a422b711f67d5fe9ce698b0ef7dd688181cf0961
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2024-03-16 15:00:19 +0200

    Create admin.py

--- dish_manager/admin.py
+++ dish_manager/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Cook, DishType, Dish, Ingredient
 
-# Register your models here.
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("dishtype",)
+    filter_horizontal = ("cooks",)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    filter_horizontal = ("dishes",)
