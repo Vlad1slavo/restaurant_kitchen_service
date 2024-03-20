392d2bb24c81053ccbd4b569e0ba13467b4789bb
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2024-03-20 13:00:36 +0200

    Update settings & requirements.txt

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0nubc@)ay=ib1f&8ilze2k9ku7p0rj&1(k_*isuh(er*qgku6q"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-0nubc@)ay=ib1f&8ilze2k9ku7p0rj&1(k_*isuh(er*qgku6q")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -112,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "dish_manager.Cook"
 
-LOGIN_REDIRECT_URL = ""
+LOGIN_REDIRECT_URL = "/"
 
 
 # Internationalization
